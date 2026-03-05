e9d3c9d92af6a505d68072fd03c33f99992b1391
David Fernandes <davidsf@pantherify.com>
2026-03-05 16:59:34 +0000

    fix: restructure vuetify initialization to include theme configuration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,12 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({ components, directives })
+const vuetify = createVuetify({
+	components,
+	directives,
+	theme: {
+		defaultTheme: 'dark',
+	},
+})
 
 createApp(App).use(vuetify).mount('#app')
